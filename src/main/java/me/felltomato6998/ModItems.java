51b32d9f92742c972d5c7f6e19899ab3b2ba3d3b
FellTomato6998 <oliverback.robaczewskiup@gmail.com>
2026-02-26 16:51:00 +0000

    poisonous_apple added

--- src/main/java/me/felltomato6998/ModItems.java
+++ src/main/java/me/felltomato6998/ModItems.java
@@ -11,6 +11,9 @@ import net.minecraft.world.item.Item;
 
 import java.util.function.Function;
 
+import static me.felltomato6998._Fabric_Documentation_.POISON_FOOD_COMPONENT;
+import static me.felltomato6998._Fabric_Documentation_.POISON_FOOD_CONSUMABLE_COMPONENT;
+
 public class ModItems {
     public static <T extends Item> T register(String name, Function<Item.Properties, T> itemFactory, Item.Properties settings) {
         // Create the item key.
@@ -25,6 +28,11 @@ public class ModItems {
         return item;
     }
     public static final Item SUSPICIOUS_SUBSTANCE = register("suspicious_substance", Item::new, new Item.Properties());
+    public static final Item POISONOUS_APPLE = register(
+            "poisonous_apple",
+            Item::new,
+            new Item.Properties().food(POISON_FOOD_COMPONENT, POISON_FOOD_CONSUMABLE_COMPONENT)
+    );
 
 
     public static void initialize() {
@@ -32,5 +40,8 @@ public class ModItems {
         // And register an event handler that adds our suspicious item to the ingredients group.
         ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.INGREDIENTS)
                 .register((itemGroup) -> itemGroup.accept(ModItems.SUSPICIOUS_SUBSTANCE));
+
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FOOD_AND_DRINKS)
+                .register((itemGroup) -> itemGroup.accept(ModItems.POISONOUS_APPLE));
     }
 }
--- src/main/java/me/felltomato6998/_Fabric_Documentation_.java
+++ src/main/java/me/felltomato6998/_Fabric_Documentation_.java
@@ -4,16 +4,26 @@ import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
 import net.fabricmc.fabric.api.registry.FuelRegistryEvents;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.component.Consumable;
+import net.minecraft.world.item.component.Consumables;
+import net.minecraft.world.item.consume_effects.ApplyStatusEffectsConsumeEffect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class _Fabric_Documentation_ implements ModInitializer {
 	public static final String MOD_ID = "ffabric_ddocumentation";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static final Consumable POISON_FOOD_CONSUMABLE_COMPONENT = Consumables.defaultFood()
+			// The duration is in ticks, 20 ticks = 1 second
+			.onConsume(new ApplyStatusEffectsConsumeEffect(new MobEffectInstance(MobEffects.POISON, 6 * 20, 1), 1.0f))
+			.build();
+	public static final FoodProperties POISON_FOOD_COMPONENT = new FoodProperties.Builder()
+			.alwaysEdible()
+			.build();
 
 	@Override
 	public void onInitialize() {
