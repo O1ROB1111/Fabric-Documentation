dbf23d3ab409ede0f8b974c8303995764743b038
FellTomato6998 <oliverback.robaczewskiup@gmail.com>
2026-02-26 15:59:12 +0000

    Initial commit

---
+++ src/client/java/me/felltomato6998/_Fabric_Documentation_Client.java
@@ -0,0 +1,10 @@
+package me.felltomato6998;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class _Fabric_Documentation_Client implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
